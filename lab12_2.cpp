7fb15ea901a1e184a042071f57e68c41e9c00ab4
aumrcn <aumkung20728@gmail.com>
2021-01-19 20:11:27 +0700

    lab12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -15,11 +15,37 @@ int main()
     int s,x,y;
     do{
         showImage(image);
-        cout << "Input your brush size and location:";
+        cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
-    }while(s != 0 && x != 0 && y != 0 );
-             return 0;    
+    }while(s != 0 || x != 0 || y != 0);
+    return 0;
+}
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0 ; i < N ; ++i){
+        cout << "|";
+        for(int j = 0 ; j < M ; ++j){
+            if(image[i][j]){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+    return;
 }
 
-// Write definition of updateImage() and showImage() here
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int i = 0 ; i < N ; ++i){
+        for(int j = 0 ; j < M ; ++j){
+            if(pow(pow((i-x),2)+pow((j-y),2),0.5) <= s-1){
+                image[i][j] = 1;
+            }
+        }
+    }
+    return;
+}
