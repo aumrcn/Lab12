35abfff6aedd459615b47360f64c71577ee30a81
aumrcn <aumkung20728@gmail.com>
2021-01-19 16:11:24 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -2,19 +2,54 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double []);
+void stat(const double[],int,double[]);
 
-int main()
-{
-    double A[] = {1.2,-3.5,6.9,7.8,12.5,-0.5};
+int main(){
+    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
     int N = sizeof(A)/sizeof(A[0]);
-    double B[4];
+    double B[6];
     stat(A,N,B);
-    cout << "Average = " << B[0];
-    cout << "\nS.D. = " << B[1];
-    cout << "\nMax = " << B[2];
-    cout << "\nMin = " << B[3];
+    cout << "Arithmetic Mean = " << B[0];
+    cout << "\nStandard Deviation = " << B[1];
+    cout << "\nGeometric Mean = " << B[2];
+    cout << "\nHarmonic Mean = " << B[3];
+    cout << "\nMax = " << B[4];
+    cout << "\nMin = " << B[5];
     return 0;
 }
 
-//Write definition of stat() here 
+void stat(const double arrone[] , int N , double arrtwo[]){
+    double ans = 0;
+    double ximinusxbar[N];
+    for(int i = 0 ; i < N ; ++i){
+        ans += arrone[i];
+    }
+    arrtwo[0] = ans / N;
+    ans = 0;
+    for(int i = 0 ; i < N ; ++i){
+        ximinusxbar[i] = pow(arrone[i] - arrtwo[0],2);
+        ans += ximinusxbar[i];
+    }
+    arrtwo[1] = pow(ans/N,0.5);
+    ans = 1;
+    for(int i = 0 ; i < N ; ++i){
+        ans = ans * arrone[i];
+    }
+    arrtwo[2] = pow(ans,(1.0/N));
+    ans = 0;
+    for(int i = 0 ; i < N ; ++i){
+        double tuasuan = 0;
+        tuasuan = 1/arrone[i]; 
+        ans = ans + tuasuan;
+    }
+    arrtwo[3] = N/ans;
+    double min = arrone[0], max = arrone[0];
+    for(int i = 0 ; i < N ; ++i){
+        if(arrone[i] > max) max = arrone[i];
+        if(arrone[i] < min) min = arrone[i];
+    }
+    arrtwo[4] = max;
+    arrtwo[5] = min;
+
+    return;
+}
