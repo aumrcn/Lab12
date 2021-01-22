92fa280429f75e77ea7b0f0e1e462a15902dd2f2
aumrcn <aumkung20728@gmail.com>
2021-01-22 19:52:28 +0700

    lab12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,49 +1,60 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
 using namespace std;
 
-const int N = 30;
-const int M = 70;
+const int N = 5;
 
-void updateImage(bool [][M],int,int,int);
+void inputMatrix(double [][N]);
 
-void showImage(const bool [][M]);
+void findLocalMax(const double [][N], bool [][N]);
 
-int main()
-{
-    bool image[N][M] = {};
-    int s,x,y;
-    do{
-        showImage(image);
-        cout << "Input your brush size and location: ";
-        cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
-    return 0;
+void showMatrix(const bool [][N]);
+
+int main(){
+	double A[N][N]; 
+	bool B[N][N]; 
+	cout << "Input the matrix.....\n";
+	inputMatrix(A);
+	findLocalMax(A,B);
+	cout << "Result is \n";
+	showMatrix(B);
+	return 0;
 }
-void showImage(const bool image[N][M]){
-    cout << "------------------------------------------------------------------------" << endl;
-    for(int i = 0 ; i < N ; ++i){
-        cout << "|";
-        for(int j = 0 ; j < M ; ++j){
-            if(image[i][j]){
-                cout << "*";
-            }else{
-                cout << " ";
-            }
+
+void showMatrix(const bool bobo[N][N]){
+    int i,j;
+    for(i = 0; i<N; ++i){
+        for(j = 0 ; j<N; ++j){
+            cout << bobo[i][j] << " ";
         }
-        cout << "|";
-        cout << endl;
+    cout << endl;
     }
-    cout << "------------------------------------------------------------------------" << endl;
     return;
 }
 
-void updateImage(bool image[][M],int s,int x,int y){
+void inputMatrix(double bobo[][N]){
+    int i,j;
+    for(i = 0 ; i < N ; ++i){
+        cout << "Row " << i+1 << ": ";
+        for(j = 0 ; j < N ; ++j){
+            cin >> bobo[i][j];
+        }    
+    }
+    return;
+}
+
+void findLocalMax(const double arr1[][N], bool arr2[][N]){
     for(int i = 0 ; i < N ; ++i){
-        for(int j = 0 ; j < M ; ++j){
-            if(pow(pow((i-x),2)+pow((j-y),2),0.5) <= s-1){
-                image[i][j] = 1;
+        for(int j = 0 ; j < N ; ++j){
+            arr2[i][j] = 0;
+        }
+    }
+    for(int i = 1 ; i < N-1 ; ++i){
+        for(int j = 1 ; j < N-1 ; ++j){
+            if(arr1[i][j] >= arr1[i - 1][j] && 
+            arr1[i][j] >= arr1[i][j + 1] && 
+            arr1[i][j] >= arr1[i + 1][j] && 
+            arr1[i][j] >= arr1[i][j - 1]){
+                arr2[i][j] = 1;
             }
         }
     }
